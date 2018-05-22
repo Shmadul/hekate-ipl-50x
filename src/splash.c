11ad09a1338d834bee09d7e7d6bd8dab2a979f83
Steven Mattera <stevenmattera@gmail.com>
2018-05-22 07:54:02 -0400

    Fixed a few bugs.

--- src/splash.c
+++ src/splash.c
@@ -39,16 +39,22 @@ flist_t * read_splashes_from_directory(char * directory) {
     FILINFO fno;
     DIR dp;
 
+    // Open the directory
     res = f_opendir(&dp, directory);
     if (res != FR_OK) {
         return NULL;
     }
 
+    // Loop through the files in the folder.
     for (;;) {
         res = f_readdir(&dp, &fno);
+
+        // Break the loop
         if (res != FR_OK || fno.fname[0] == 0) {
             break;
-        } else if (fno.fname == "." || fno.fname == "..") {
+        }
+        // Skip directories.
+        else if (fno.fattrib & AM_DIR) {
             continue;
         }
 
@@ -65,16 +71,20 @@ flist_t * read_splashes_from_directory(char * directory) {
         numberOfSplashes++;
     }
 
+    // Clean up.
     f_closedir(&dp);
+
     return head;
 }
 
 char * randomly_choose_splash(flist_t * head, char * directory) {
     flist_t * current = NULL;
 
+    // Seed the randomization and choose our lucky winner.
     srand(get_tmr());
     int splashChosen = rand() % *head->length;
 
+    // Go through our linked list and get the chosen splash.
     for (int i = 0; i <= splashChosen; i++) {
         if (current == NULL) {
             current = head;
@@ -83,6 +93,7 @@ char * randomly_choose_splash(flist_t * head, char * directory) {
         }
     }
 
+    // Construct filename.
     char filename[sizeof(current->name) + 9];
     strcpy(filename, directory);
     strcat(filename, "/");
@@ -93,14 +104,22 @@ int write_splash_to_framebuffer(gfx_con_t * con, char * filename) {
     FIL fp;
     char buffer[SPLASH_SIZE + 1];
 
+    // Open the file.
     if (f_open(&fp, filename, FA_READ) == FR_OK) {
+        // Read the file to the buffer.
 	    f_read(&fp, buffer, SPLASH_SIZE, NULL);
+
+        // Copy the splash from our buffer to the framebuffer.
         memcpy(con->gfx_ctxt->fb, buffer + 1, SPLASH_SIZE);
+
+        // Clean up.
         f_close(&fp);
         
+        // Success
         return 1;
     }
 
+    // Failure
     return 0;
 }
 
@@ -110,6 +129,10 @@ void draw_splash(gfx_con_t * con) {
     }
 
     flist_t * head = read_splashes_from_directory("splashes");
+    if (head == NULL) {
+        return;
+    }
+
     char * filename = randomly_choose_splash(head, "splashes");
     write_splash_to_framebuffer(con, filename);
 }
