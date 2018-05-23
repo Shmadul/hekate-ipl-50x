3817c60195c49a927c86dd2fbae2b11789bcff79
Steven Mattera <stevenmattera@gmail.com>
2018-05-23 07:50:48 -0400

    Updated version.

--- src/splash.c
+++ src/splash.c
@@ -102,7 +102,7 @@ flist_t * read_splashes_from_directory(char * directory) {
 
 void print_header(gfx_con_t * con) {
     static const char switchblade[] =
-		"SwitchBlade v1.0.5 - By StevenMattera and Shmadul\n\n"
+		"SwitchBlade v1.0.6 - By StevenMattera and Shmadul\n\n"
 		"Based on the awesome work of naehrwert, st4rk\n"
 		"Thanks to: derrek, nedwill, plutoo, shuffle2, smea, thexyz, yellows8\n"
 		"Greetings to: fincs, hexkyz, SciresM, Shiny Quagsire, WinterMute\n"
